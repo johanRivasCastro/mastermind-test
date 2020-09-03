3c8bfd8f2446b544cbce553271b5e24a524e2432
johanrivascastro <johanrivascastro@gmail.com>
2020-09-03 14:08:54 -0400

    Updates

---
+++ precache-manifest.4e927699474aa0971ccf7c897ed0683f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47787600f4211ca847b5f7844478538b",
+    "url": "/mastermind-test/index.html"
+  },
+  {
+    "revision": "a4f0ff101f2b129b8162",
+    "url": "/mastermind-test/static/js/2.0fa7c689.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/mastermind-test/static/js/2.0fa7c689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45189cc07798c423ffba",
+    "url": "/mastermind-test/static/js/main.d916fbc8.chunk.js"
+  },
+  {
+    "revision": "f96126f016480f5d6d77",
+    "url": "/mastermind-test/static/js/runtime-main.2f121cb3.js"
+  }
+]);
